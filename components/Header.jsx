0184fdc3a590e2139b5630fb8c5b35ece95d605d
Florian Durand <contact@floriandurand.com>
2020-06-27 17:44:03 +0200

    add propType

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,16 +1,23 @@
 import Link from 'next/link';
+import PropTypes from 'prop-types';
 
 import Typography from '@material-ui/core/Typography';
 import Breadcrumbs from '@material-ui/core/Breadcrumbs';
 import LinkStyle from '@material-ui/core/Link';
 
-const Header = (props) => (
-  <Breadcrumbs aria-label="breadcrumb">
-    <Link href="/">
-      <LinkStyle color="inherit">Characters</LinkStyle>
-    </Link>
-    <Typography color="textPrimary">{props.name}</Typography>
-  </Breadcrumbs>
-);
+const Header = (props) => {
+  const { name } = props;
+  return (
+    <Breadcrumbs aria-label="breadcrumb">
+      <Link href="/">
+        <LinkStyle color="inherit">Characters</LinkStyle>
+      </Link>
+      <Typography color="textPrimary">{name}</Typography>
+    </Breadcrumbs>
+  );
+};
 
+Header.propTypes = {
+  name: PropTypes.string.isRequired,
+};
 export default Header;
--- components/List.jsx
+++ components/List.jsx
@@ -1,28 +1,43 @@
+import PropTypes from 'prop-types';
+
 import { ListItem, ListItemText, Typography } from '@material-ui/core';
 import ListStyle from '@material-ui/core/List';
 
-const List = (props) => (
-  <div>
-    <Typography variant="h6" noWrap>
-      Here you can find all the
-      {' '}
-      {props.type}
-      {' '}
-      where
-      {' '}
-      {props.name}
-      {' '}
-      appears :
-    </Typography>
-    <ListStyle>
-      {props.value.items.map((single, index) => (
-        <ListItem key={`${props.type}-${index}`}>
-          <ListItemText primary={single.name} />
-        </ListItem>
-      ))}
-    </ListStyle>
+const List = (props) => {
+  const { name, value, type } = props;
+  const { items } = value;
+  return (
+    <div>
+      <Typography variant="h6" noWrap>
+        Here you can find all the
+        {' '}
+        {type}
+        {' '}
+        where
+        {' '}
+        {name}
+        {' '}
+        appears :
+      </Typography>
+      <ListStyle>
+        {items.map((single, index) => (
+          <ListItem key={`${type}-${index}`}>
+            <ListItemText primary={single.name} />
+          </ListItem>
+        ))}
+      </ListStyle>
 
-  </div>
-);
+    </div>
+  );
+};
 
+List.defaultProps = {
+  value: { items: [] },
+};
+
+List.propTypes = {
+  name: PropTypes.string.isRequired,
+  value: PropTypes.objectOf(PropTypes.any),
+  type: PropTypes.string.isRequired,
+};
 export default List;
