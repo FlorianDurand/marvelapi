1cd5ddfd1c0bd32180f2af7735236db828b8747a
Florian Durand <contact@floriandurand.com>
2020-06-27 20:03:34 +0200

    update test

--- cypress/integration/single.spec.js
+++ cypress/integration/single.spec.js
@@ -1,9 +1,9 @@
-const li = Math.floor(Math.random() * Math.floor(15));
+const a = Math.floor(Math.random() * Math.floor(15));
 
 describe('SingleCharacter', () => {
   it('Une page personnage doit s\'ouvrir', () => {
     cy.visit('http://localhost:3000');
-    cy.get('li').eq(li).click();
+    cy.get('a').eq(a).should('have.attr', 'href').then(((href) => { cy.visit(`http://localhost:3000${href}`); }));
     cy.get('h1').should('be.visible');
   });
 });
